c0d6f74f473402fe7ede9b671684c691dc514b94
krunal dhavle <krunaldhavle1999@gmail.com>
2024-03-27 08:22:11 +0000

    find doctor profile

--- src/routes/doctor.routes.js
+++ src/routes/doctor.routes.js
@@ -10,6 +10,8 @@ import {
 const doctorRouter = Router();
 
 doctorRouter.route("/").get(getAllDoctors);
+doctorRouter.route("/:id").get(getDoctorById);
+
 
 doctorRouter.route("/profile/:id").put(verifyJWT, doctorAuth, updateDoctor);
 doctorRouter.route("/profile/me/:id").get(verifyJWT, doctorAuth, getDoctorById);
